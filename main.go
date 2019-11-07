f310899f0ba081b789186355354f7acb0eff23e9
fenx91 <fenx91@gmail.com>
2019-11-07 21:39:47 +0800

    remove /.idea

--- main.go
+++ main.go
@@ -1,2 +1,104 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+var users = map[string]string{
+	"fenxy": "password1",
+}
+
+var jwtSecret = []byte("my_secret_key")
+
+type Credentials struct {
+	Password string `json:"password"`
+	Username string `json:"username"`
+}
+
+type Claims struct {
+	Username string `json:"username"`
+	jwt.StandardClaims
+}
+
+func SignInHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("entered signin handler")
+	var credentials Credentials
+	err := json.NewDecoder(r.Body).Decode(&credentials)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Println("bad request 1")
+		return
+	}
+	fmt.Println(credentials)
+
+	expectedPassword, ok := users[credentials.Username]
+
+	if !ok || expectedPassword != credentials.Password {
+		w.WriteHeader(http.StatusUnauthorized)
+		fmt.Fprintf(w, "Wwong passowrd or username")
+		return
+	}
+	claims := &Claims{
+		Username:       credentials.Username,
+		StandardClaims: jwt.StandardClaims{},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString(jwtSecret)
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+
+	http.SetCookie(w, &http.Cookie{
+		Name:  "token",
+		Value: tokenString,
+	})
+}
+
+func WelcomeHandler(w http.ResponseWriter, r *http.Request) {
+	c, err := r.Cookie("token")
+	if err != nil {
+		if err == http.ErrNoCookie {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	tokenString := c.Value
+	claims := &Claims{}
+
+	token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
+		return jwtSecret, nil
+	})
+
+	if err != nil {
+		if err == jwt.ErrSignatureInvalid {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	if token.Valid == false {
+		w.WriteHeader(http.StatusUnauthorized)
+	}
+
+	fmt.Fprintf(w, "Welcome %s", claims.Username)
+
+}
+
+func main() {
+	fmt.Println("runnign")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) { fmt.Fprintf(w, "hello") })
+	http.HandleFunc("/signin", SignInHandler)
+	http.HandleFunc("/welcome", WelcomeHandler)
+	log.Fatal(http.ListenAndServe(":10001", nil))
+}
