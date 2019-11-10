506f2eb244714b3d47390b5aafd6a4f0ad5eba12
Xiao Feng <fenx91@gmail.com>
2019-11-11 01:34:44 +0800

    added jwt login logic

--- main.go
+++ main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	"encoding/json"
+	"errors"
 	"fmt"
+	"github.com/dgrijalva/jwt-go"
+	"html/template"
 	"log"
 	"net/http"
-
-	"github.com/dgrijalva/jwt-go"
 )
 
 var users = map[string]string{
@@ -25,14 +25,33 @@ type Claims struct {
 	jwt.StandardClaims
 }
 
+type TemplateData struct {
+	Username string
+}
+
+func setTokenCookie(w http.ResponseWriter, token string) {
+	http.SetCookie(w, &http.Cookie{
+		Name:  "token",
+		Value: token,
+	})
+}
+
+func deleteTokenCookie(w http.ResponseWriter) {
+	http.SetCookie(w, &http.Cookie{
+		Name:   "token",
+		MaxAge: 0,
+	})
+}
+
 func SignInHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("entered signin handler")
-	var credentials Credentials
-	err := json.NewDecoder(r.Body).Decode(&credentials)
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		fmt.Println("bad request 1")
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
 		return
+	}
+	fmt.Println("entered signin handler")
+	credentials := &Credentials{
+		r.FormValue("password"),
+		r.FormValue("username"),
 	}
 	fmt.Println(credentials)
 
@@ -55,50 +74,71 @@ func SignInHandler(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusInternalServerError)
 	}
 
-	http.SetCookie(w, &http.Cookie{
-		Name:  "token",
-		Value: tokenString,
-	})
+	setTokenCookie(w, tokenString)
+	//fmt.Fprintf(w, "login succeed. <a href=\"/\"> Go to homepage</a>")
+	http.Redirect(w, r, "/", http.StatusFound)
 }
 
-func WelcomeHandler(w http.ResponseWriter, r *http.Request) {
+func VerifyAndGetUsername(r *http.Request) (Username string, err error) {
 	c, err := r.Cookie("token")
 	if err != nil {
-		if err == http.ErrNoCookie {
-			w.WriteHeader(http.StatusUnauthorized)
-			return
-		}
-		w.WriteHeader(http.StatusBadRequest)
-		return
+		return "", err
 	}
 
 	tokenString := c.Value
 	claims := &Claims{}
 
-	token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
-		return jwtSecret, nil
-	})
+	token, err := jwt.ParseWithClaims(tokenString, claims,
+		func(token *jwt.Token) (interface{}, error) {
+			return jwtSecret, nil
+		})
 
 	if err != nil {
-		if err == jwt.ErrSignatureInvalid {
-			w.WriteHeader(http.StatusUnauthorized)
-			return
-		}
-		w.WriteHeader(http.StatusBadRequest)
-		return
+		return "", err
 	}
 	if token.Valid == false {
-		w.WriteHeader(http.StatusUnauthorized)
+		return "", errors.New("token not valid")
 	}
 
-	fmt.Fprintf(w, "Welcome %s", claims.Username)
+	return claims.Username, nil
+}
 
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	username, err := VerifyAndGetUsername(r)
+
+	if err != nil {
+		http.Redirect(w, r, "/loginpage", http.StatusFound)
+		return
+	}
+
+	t, _ := template.ParseFiles("html/index.html")
+
+	_ = t.Execute(w, &TemplateData{
+		Username: username,
+	})
+}
+
+func LogInPageHandler(w http.ResponseWriter, r *http.Request) {
+	t, _ := template.ParseFiles("html/login.html")
+
+	_ = t.Execute(w, nil)
+}
+
+func LogOutHandler(w http.ResponseWriter, r *http.Request) {
+	deleteTokenCookie(w)
+	fmt.Fprintf(w, "Logout succeed")
 }
 
 func main() {
-	fmt.Println("runnign")
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) { fmt.Fprintf(w, "hello") })
-	http.HandleFunc("/signin", SignInHandler)
-	http.HandleFunc("/welcome", WelcomeHandler)
+	http.HandleFunc("/", IndexHandler)
+	http.HandleFunc("/signinaction", SignInHandler)
+	http.HandleFunc("/loginpage", LogInPageHandler)
+	http.HandleFunc("/logout", LogOutHandler)
+	fmt.Println("running server on :10001")
 	log.Fatal(http.ListenAndServe(":10001", nil))
 }
